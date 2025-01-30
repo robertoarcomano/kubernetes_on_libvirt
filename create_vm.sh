2dd1994247b759f88f3fb3313598e9f7175c3c0e
Roberto Arcomano <info@robertoarcomano.com>
2025-01-30 22:29:24 +0100

    Successfully get the kube config file, with the hostname updated.

--- create_vm.sh
+++ create_vm.sh
@@ -2,5 +2,6 @@
 ./create_key.sh
 date > log
 vagrant up >> log 2>&1
+vagrant ssh master -c "sudo cat /etc/kubernetes/admin.conf" | sed 's|server: https://[^:]*:6443|server: https://master:6443|' > kubeconfig
 date >> log
 
